f6a663b1f45614f621135c4a96c7844282124234
Vijaykumar-03 <paxsaat03@gmail.com>
2026-07-28 20:56:38 +0530

    Update main.tf

--- Devops/Modules/Network/main.tf
+++ Devops/Modules/Network/main.tf
@@ -9,3 +9,161 @@ resource "aws_vpc" "main" {
   }
 
 }
+resource "aws_internet_gateway" "igw" {
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+
+}
+resource "aws_subnet" "public1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_1_cidr
+
+  availability_zone = "${var.region}a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-1"
+  }
+
+}
+resource "aws_subnet" "public2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_2_cidr
+
+  availability_zone = "${var.region}b"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-2"
+  }
+
+}
+resource "aws_subnet" "private1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_1_cidr
+
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.project_name}-private-1"
+  }
+
+}
+resource "aws_subnet" "private2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_2_cidr
+
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "${var.project_name}-private-2"
+  }
+
+}
+resource "aws_eip" "nat" {
+
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-eip"
+  }
+
+}
+resource "aws_nat_gateway" "nat" {
+
+  allocation_id = aws_eip.nat.id
+
+  subnet_id = aws_subnet.public1.id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+
+}
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.igw.id
+
+  }
+
+  tags = {
+
+    Name = "${var.project_name}-public-rt"
+
+  }
+
+}
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+
+    nat_gateway_id = aws_nat_gateway.nat.id
+
+  }
+
+  tags = {
+
+    Name = "${var.project_name}-private-rt"
+
+  }
+
+}
+resource "aws_route_table_association" "public1" {
+
+  subnet_id = aws_subnet.public1.id
+
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_route_table_association" "public2" {
+
+  subnet_id = aws_subnet.public2.id
+
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_route_table_association" "private1" {
+
+  subnet_id = aws_subnet.private1.id
+
+  route_table_id = aws_route_table.private.id
+
+}
+
+resource "aws_route_table_association" "private2" {
+
+  subnet_id = aws_subnet.private2.id
+
+  route_table_id = aws_route_table.private.id
+
+}
