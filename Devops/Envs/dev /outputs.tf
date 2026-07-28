57a2e4e77ea438c78da8eca52c40bcdec62bb131
Vijaykumar-03 <paxsaat03@gmail.com>
2026-07-28 21:54:21 +0530

    Update outputs.tf

--- Devops/Envs/dev /outputs.tf
+++ Devops/Envs/dev /outputs.tf
@@ -6,10 +6,6 @@ output "alb_dns_name" {
   value = module.ecs.alb_dns_name
 }
 
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
-
 output "rds_endpoint" {
   value = module.rds.rds_endpoint
 }
