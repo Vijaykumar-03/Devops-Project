62904a77ab051dbd37f2de8cae4933eda4461a37
Vijaykumar-03 <paxsaat03@gmail.com>
2026-07-28 21:33:20 +0530

    Update main.tf

--- Devops/Envs/dev /main.tf
+++ Devops/Envs/dev /main.tf
@@ -17,7 +17,7 @@ module "network" {
 }
 module "ecs" {
 
-  source = "../../modules/ecs"
+  source = "../../Modules/Ecs"
 
   project_name = var.project_name
 
@@ -42,7 +42,7 @@ module "ecs" {
 }
 module "rds" {
 
-  source = "../../modules/rds"
+  source = "../../Modules/Rds"
 
   project_name = var.project_name
 
